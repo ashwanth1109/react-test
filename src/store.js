f381ff050712c3afa93f8a8d75491dc6c89a10ef
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-15 19:14:18 +0530

    Added sortParameter and menuState Reducers

--- src/store.js
+++ src/store.js
@@ -22,8 +22,26 @@ const buttonLoading = (state = false, action) =>
     action.type === "SET_BUTTON_LOADING" ? action.buttonLoading : state;
 
 // ------------------------------------------------------------
+// Sort Parameter Reducer
+// ------------------------------------------------------------
+const sortParameter = (state = 3, action) =>
+    action.type === "SORT_PARAMETER" ? action.sortParameter : state;
+
+// ------------------------------------------------------------
+// Menu State Reducer
+// ------------------------------------------------------------
+const menuState = (state = false, action) =>
+    action.type === "MENU_STATE" ? action.menuState : state;
+
+// ------------------------------------------------------------
 // Export Store
 // ------------------------------------------------------------
 export default createStore(
-    combineReducers({ userList, buttonLoading, selectedUser })
+    combineReducers({
+        userList,
+        buttonLoading,
+        selectedUser,
+        sortParameter,
+        menuState
+    })
 );
