7652581979e1bb25df67c0edd562ed99ccf50f40
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-15 12:21:51 +0530

    App is integrated with redux provider and react router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import store from "./store";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import IndexPage from "./pages/index";
+import ShowPage from "./pages/show";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Provider store={store}>
+                <React.Fragment>
+                    <Router>
+                        <React.Fragment>
+                            <Route exact path="/" component={IndexPage} />
+                            <Route path="/user" component={ShowPage} />
+                        </React.Fragment>
+                    </Router>
+                </React.Fragment>
+            </Provider>
+        );
+    }
 }
 
 export default App;
